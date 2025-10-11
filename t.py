0436ea2bdd8c8f5409d0eb07aac64a46f38d14bc
MSharifM <msharifmdev@gmail.com>
2025-10-11 15:18:50 +0330

    Update t.py

--- t.py
+++ t.py
@@ -6,6 +6,10 @@ def example_write():
     # 1 = stdout
     os.write(1, b"hello from os.write()\n")
 
+def meta_data():
+    info = os.stat("t.py")
+    print("size:", info.st_size, "bytes")
+
 def example_create_file():
     fd = os.open("testfile.txt", os.O_CREAT | os.O_WRONLY | os.O_TRUNC, 0o644)
     os.write(fd, b"This was written by os.open/os.write/os.close\n")
@@ -26,7 +30,7 @@ def example_fork_exec():
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("Usage: python3 syscall_examples.py [write|file|forkexec]")
+        print("Usage: python3 syscall_examples.py [write|file|forkexec|metadata]")
         sys.exit(1)
     cmd = sys.argv[1]
     if cmd == "write":
@@ -35,5 +39,7 @@ if __name__ == "__main__":
         example_create_file()
     elif cmd == "forkexec":
         example_fork_exec()
+    elif cmd == "metadata":
+        meta_data()
     else:
         print("unknown")
